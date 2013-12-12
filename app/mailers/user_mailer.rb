8d6b177b39d08a765b14aa860770180e1cf7d193
Lane Davis <lanedavis.csl@gmail.com>
2013-12-12 08:11:05 +0000

    sending mail

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,8 @@
+class UserMailer < ActionMailer::Base
+  default from: "do-not-reply@example.com"
+  
+  def contact_email(contact)
+    @contact = contact
+    mail(to: ENV["GMAIL_USERNAME"], from: @contact.email, :subject => "Website Contact")
+  end
+end
